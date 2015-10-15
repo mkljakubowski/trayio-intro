afc5f7e9fa247e0d06050bcc434cf16326b38ea6
Mikołaj Jakubowski <mjakubowski@virtuslab.com>
2015-10-15 22:42:17 +0200

    create Room, build file, test input data

---
+++ src/main/scala/io/tray/intro/Room.scala
@@ -0,0 +1,60 @@
+package io.tray.intro
+
+import scala.util.Try
+
+case class Position(x: Int, y: Int)
+
+sealed trait Instruction
+
+case object North extends Instruction
+case object South extends Instruction
+case object West extends Instruction
+case object East extends Instruction
+
+object Instruction {
+  def apply(l: Char): Instruction = l match {
+    case "N" => North
+    case "S" => South
+    case "W" => West
+    case "E" => East
+  }
+
+  val allowedChars = "NSEW"
+}
+
+case class Room(dirtPatches: Seq[Position], width: Int, height: Int, instructions: Seq[Instruction])
+
+object Room {
+
+  private def validateInput(data: String): Boolean = {
+    val lines = data.split("\n")
+    if(lines.nonEmpty){
+      false
+    } else {
+      val numberTuples = lines.init.forall{ line =>
+        val maybeInts = line.split(" ")
+        (maybeInts.size == 2) && maybeInts.forall{maybeInt => Try(maybeInt.toInt).isSuccess}
+      }
+      val instructions = lines.last.forall(l => Instruction.allowedChars.contains(l))
+      instructions && numberTuples
+    }
+  }
+
+  def create(data: String): Option[Room] = {
+    if(validateInput(data)) {
+      val lines = data.split("\n")
+      val linesAsTuples = lines.map { line =>
+        val ints = line.split(" ").map(_.toInt)
+        ints(0) -> ints(1)
+      }
+      val (width, height) = linesAsTuples.head
+      val patches = linesAsTuples.tail.init.map(Function.tupled(Position.apply))
+      val instructions = lines.last.map(Instruction.apply)
+      val room = Room(patches, width, height, instructions)
+      Some(room)
+    } else {
+      None
+    }
+  }
+
+}
